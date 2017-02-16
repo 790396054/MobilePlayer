f13ad28e62f04a6758cbdecb86f7c636129a5dd5
gmm <miaomiaogong92@gmail.com>
2017-02-16 23:48:25 +0800

    框架搭建

--- app/src/main/java/com/app/gmm/mobileplyer/activity/MainActivity.java
+++ app/src/main/java/com/app/gmm/mobileplyer/activity/MainActivity.java
@@ -3,6 +3,8 @@ package com.app.gmm.mobileplyer.activity;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.app.gmm.mobileplyer.R;
@@ -13,9 +15,18 @@ import com.app.gmm.mobileplyer.R;
  */
 
 public class MainActivity extends Activity{
+    private FrameLayout fl_main_content;
+
+    private RadioGroup rg_bottom_tag;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fl_main_content = (FrameLayout) findViewById(R.id.fl_main_content);
+        rg_bottom_tag = (RadioGroup) findViewById(R.id.rg_bottom_tag);
+
+        rg_bottom_tag.check(R.id.rb_video);
     }
 }
