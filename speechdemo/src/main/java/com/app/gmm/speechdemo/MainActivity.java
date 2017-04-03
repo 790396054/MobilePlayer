79df757850f62da1fbfd05151976a72a3effb268
gmm <miaomiaogong92@gmail.com>
2017-04-03 19:09:44 +0800

    完成音乐播放器

---
+++ speechdemo/src/main/java/com/app/gmm/speechdemo/MainActivity.java
@@ -0,0 +1,134 @@
+package com.app.gmm.speechdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.iflytek.cloud.ErrorCode;
+import com.iflytek.cloud.InitListener;
+import com.iflytek.cloud.RecognizerResult;
+import com.iflytek.cloud.SpeechConstant;
+import com.iflytek.cloud.SpeechError;
+import com.iflytek.cloud.SpeechUtility;
+import com.iflytek.cloud.ui.RecognizerDialog;
+import com.iflytek.cloud.ui.RecognizerDialogListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText mEditText;
+    private Button mBtnVoice;
+    private Button mBtnWrite;
+
+    // 用HashMap存储听写结果
+    private HashMap<String, String> mIatResults = new LinkedHashMap<String, String>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // 将“12345678”替换成您申请的 APPID，申请地址： http://www.xfyun.cn
+        // 请勿在“ =” 与 appid 之间添加任务空字符或者转义符
+        SpeechUtility.createUtility(this, SpeechConstant.APPID + "=5795c210");
+        mEditText = (EditText) findViewById(R.id.et_input);
+        mBtnVoice = (Button) findViewById(R.id.btn_voice);
+        mBtnWrite = (Button) findViewById(R.id.btn_write);
+        mBtnVoice.setOnClickListener(this);
+        mBtnWrite.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_voice:
+                showDialog();
+                break;
+            case R.id.btn_write:
+
+                break;
+        }
+    }
+
+    private void showDialog() {
+        //1.创建RecognizerDialog对象
+        RecognizerDialog mDialog = new RecognizerDialog(this, new MyInitListener());
+        //2.设置accent、 language等参数
+        mDialog.setParameter(SpeechConstant.LANGUAGE, "zh_cn");
+        mDialog.setParameter(SpeechConstant.ACCENT, "mandarin");
+        //若要将UI控件用于语义理解，必须添加以下参数设置，设置之后onResult回调返回将是语义理解
+        //结果
+        // mDialog.setParameter("asr_sch", "1");
+        // mDialog.setParameter("nlp_version", "2.0");
+        //3.设置回调接口
+        mDialog.setListener(new MyRecognizerDialogListener());
+        //4.显示dialog，接收语音输入
+        mDialog.show();
+    }
+
+    class MyRecognizerDialogListener implements RecognizerDialogListener {
+
+        /**
+         * @param recognizerResult
+         * @param b                是否说话结束
+         */
+        @Override
+        public void onResult(RecognizerResult recognizerResult, boolean b) {
+            String result = recognizerResult.getResultString();
+            Log.e("MainActivity", "result ==" + result);
+            String text = JsonParser.parseIatResult(result);
+            //解析好的
+            Log.e("MainActivity", "text ==" + text);
+
+            String sn = null;
+            // 读取json结果中的sn字段
+            try {
+                JSONObject resultJson = new JSONObject(recognizerResult.getResultString());
+                sn = resultJson.optString("sn");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            mIatResults.put(sn, text);
+
+            StringBuffer resultBuffer = new StringBuffer();//拼成一句
+            for (String key : mIatResults.keySet()) {
+                resultBuffer.append(mIatResults.get(key));
+            }
+
+            mEditText.setText(resultBuffer.toString());
+            mEditText.setSelection(mEditText.length());
+
+        }
+
+        /**
+         * 出错了
+         *
+         * @param speechError
+         */
+        @Override
+        public void onError(SpeechError speechError) {
+            Log.e("MainActivity", "onError ==" + speechError.getMessage());
+
+        }
+    }
+
+
+    class MyInitListener implements InitListener {
+
+        @Override
+        public void onInit(int i) {
+            if (i != ErrorCode.SUCCESS) {
+                Toast.makeText(MainActivity.this, "初始化失败", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
